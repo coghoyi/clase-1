0c96c2e74230eab6782db5e551f5e7102e06afcf
coghoyi <davidhoyolopez@alumnostrazos.net>
2023-06-05 20:20:27 +0200

    clase-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+const name = 'David';
+const number = 32;
+const boolean = false;
+const isNull = null;
+const array = [1, 2, 3, 4, 5];
+const object = { a: 1, b: 3 };
+
 const App = () => {
-	return <h1>Núcleo de la aplicación</h1>;
+	return (
+		<>
+			<h1>{name}</h1>
+			<h1>{number}</h1>
+			<h1>{boolean}</h1>
+			<h1>{isNull}</h1>
+			<h1>{array}</h1>
+			<h1>{object}</h1>
+		</>
+	);
 };
 
 export default App;
